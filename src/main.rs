217246d334614682a63514a84ec03988bbe6e163
alienzj <alienchuj@gmail.com>
2021-04-13 07:07:15 +0800

    build pass

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use clap::Clap;
-use serde_derive::Deserialize;
+use serde_derive::{Deserialize, Serialize};
 use std::fmt::Debug;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use toml;
 
 #[derive(Clap, Debug)]
 #[clap(version = "0.1", author = "alienzj <alienchuj@gmail.com>")]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
-    #[clap(short)]
+    #[clap(long)]
     verbose: bool,
-    #[clap(short)]
+    #[clap(long)]
     otp: bool,
-    #[clap(short)]
+    #[clap(long)]
     retry: bool,
 }
 
@@ -27,106 +28,144 @@ enum SubCommand {
     Download(Download),
 }
 
-#[derive(Clap, Deserialize, Debug)]
+#[derive(Clap, Debug)]
 struct Config {
-    #[clap(short, long, default_value = "~/.config/sshkit/sshkit.toml")]
+    #[clap(long, default_value = "/home/alienzj/.config/sshkit.toml")]
     config: String,
+    #[clap(long)]
     user: String,
+    #[clap(long)]
     node: String,
+    #[clap(long)]
     password: String,
+    #[clap(long)]
     code: String,
-    tunel: Vec<String>,
 }
 
 #[derive(Clap, Debug)]
 struct Login {
-    #[clap(short, long, default_value = "~/.config/sshkit/sshkit.toml")]
+    #[clap(long, default_value = "/home/alienzj/.config/sshkit.toml")]
     config: String,
+    #[clap(long)]
     user: String,
+    #[clap(long)]
     node: String,
+    #[clap(long)]
     password: String,
+    #[clap(long)]
     code: String,
 }
 
 #[derive(Clap, Debug)]
 struct Tunel {
-    #[clap(short, long, default_value = "~/.config/sshkit/sshkit.toml")]
+    #[clap(long, default_value = "/home/alienzj/.config/sshkit.toml")]
     config: String,
+    #[clap(long)]
     user: String,
+    #[clap(long)]
     node: String,
+    #[clap(long)]
     password: String,
+    #[clap(long)]
     code: String,
+    #[clap(long)]
     tunel: Vec<String>,
 }
 
 #[derive(Clap, Debug)]
 struct Upload {
-    #[clap(short, long, default_value = "~/.config/sshkit/sshkit.toml")]
+    #[clap(long, default_value = "/home/alienzj/.config/sshkit.toml")]
     config: String,
+    #[clap(long)]
     user: String,
+    #[clap(long)]
     node: String,
+    #[clap(long)]
     password: String,
+    #[clap(long)]
     code: String,
+    #[clap(long)]
     from: String,
+    #[clap(long)]
     to: String,
 }
 
 #[derive(Clap, Debug)]
 struct Download {
-    #[clap(short, long, default_value = "~/.config/sshkit/sshkit.toml")]
+    #[clap(long, default_value = "/home/alienzj/.config/sshkit.toml")]
     config: String,
+    #[clap(long)]
+    user: String,
+    #[clap(long)]
+    node: String,
+    #[clap(long)]
+    password: String,
+    #[clap(long)]
+    code: String,
+    #[clap(long)]
+    from: String,
+    #[clap(long)]
+    to: String,
+}
+#[derive(Deserialize, Debug)]
+struct Configs {
+    configs: Vec<Hosts>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Hosts {
+    host: Vec<Host>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Host {
     user: String,
     node: String,
     password: String,
     code: String,
-    from: String,
-    to: String,
-}
-
-#[derive(Deserialize, Debug)]
-struct Configs {
-    configs: Vec<Config>,
 }
 
 fn main() {
     let opts = Opts::parse();
 
     match opts.subcmd {
-        SubCommand::Config(config) => configer(config),
-        SubCommand::Login(config) => loginer(config),
-        SubCommand::Tunel(config) => tuneler(config),
-        SubCommand::Upload(config) => uploader(config),
-        SubCommand::Download(config) => downloader(config),
+        SubCommand::Config(args) => configer(args),
+        SubCommand::Login(args) => loginer(args),
+        SubCommand::Tunel(args) => tuneler(args),
+        SubCommand::Upload(args) => uploader(args),
+        SubCommand::Download(args) => downloader(args),
     }
 }
 
-fn configer(config: Config) {
-    let config_file = config.config;
-    let user_name = config.user;
-    println!("{}", config_file);
-    println!("{}", user_name);
+fn configer(args: Config) {
+    let config_file = args.config;
+    let user_name = args.user;
+    println!("{} {}", config_file, user_name);
 
     if Path::exists(Path::new(&config_file)) {
-        let mut config_handle = match File::open(&config_file) {
+        let mut handle = match File::open(&config_file) {
             Ok(s) => s,
             Err(e) => panic!("Error occurred opening file: {} - Err: {}", config_file, e),
         };
+
         let mut config_str = String::new();
-        match config_handle.read_to_string(&mut config_str) {
+        match handle.read_to_string(&mut config_str) {
             Ok(s) => s,
             Err(e) => panic!("Error reading file: {}", e),
         };
+        println!("{:?}", config_str);
 
-        let configs: Configs = toml::from_str(config_str.as_str()).unwrap();
-
-        println!("config: \n {:#?}", configs);
+        let configs: Hosts = toml::from_str(config_str.as_str()).unwrap();
+        println!("{:?}", configs);
+        //let configs_toml = toml::to_string_pretty(&configs).unwrap();
+        //println!("{:?}", configs_toml);
     }
 }
 
-fn loginer(config: Login) {}
+fn loginer(args: Login) {}
 
-fn tuneler(config: Tunel) {}
+fn tuneler(args: Tunel) {}
 
-fn uploader(config: Upload) {}
+fn uploader(args: Upload) {}
 
-fn downloader(config: Download) {}
+fn downloader(args: Download) {}
